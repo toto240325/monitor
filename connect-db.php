a462f239fe918d842d3a9474515ea451c74dcbd6
toto240325 <toto240325@gmail.com>
2019-11-14 22:47:53 +0100

    misc

---
+++ connect-db.php
@@ -0,0 +1,28 @@
+<?php
+
+if ($dbhost = '192.168.0.147')
+{
+$dbuser = 'toto';
+$dbpass = 'Toto!';
+$mydb = 'mydatabase';
+} 
+elseif ($dbhost = 'p702')
+{
+$dbuser = 'toto';
+$dbpass = 'Toto!';
+$mydb = 'mydatabase';
+} 
+elseif ($dbhost = 'aws ec2')
+{
+$dbuser = 'toto';
+$dbpass = 'Toto!';
+$mydb = 'mydatabase';
+} 
+elseif ($dbhost = 'local')
+{
+$dbuser = 'root';
+$dbpass = 'Toto!';
+$mydb = 'mydatabase';
+}	
+
+?>
---
+++ params.php
@@ -0,0 +1,6 @@
+<?php
+//$webserver = "192.168.0.147";
+$webserver = "localhost";
+$dbhost = "192.168.0.147";
+//$dbhost = "localhost";
+?>
